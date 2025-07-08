a32074ffe78346d660dc26c0f301c0e60f6f0746
rishabh-registerkaro <rishabh@registerkaro.com>
2025-07-08 23:01:39 +0530

    auth.ts file changes little bit only

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosApiInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_APP_MODE === "development" ? process.env.NEXT_PUBLIC_BASE_URL : "/",
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL, 
     withCredentials: true,
 })
